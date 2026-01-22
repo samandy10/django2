9d1b430fd223b85fee067154a05ca12f8069b031
Samir Anand <samir.anand@innlabs.com>
2026-01-22 18:21:17 +0530

    Tailwind css used,static files added,Templated based conversion

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -6,5 +6,6 @@ class Item(models.Model):
         return self.item_name
     
     item_name = models.CharField(max_length=200)
-    item_desc = models.CharField()
+    item_desc = models.CharField(max_length=1000)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default='https://imgs.search.brave.com/ly6q72bVHFGys1gJtrLNOkcfdZdZbZuIpJg36gF3bdA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly91cy4x/MjNyZi5jb20vNDUw/d20veXVwaXJhbW9z/L3l1cGlyYW1vczE0/MDcveXVwaXJhbW9z/MTQwNzAyNDE0LzMw/MjE4ODMzLWZvb2Qt/ZGVzaWduLW92ZXIt/YmVpZ2UtYmFja2dy/b3VuZC12ZWN0b3It/aWxsdXN0cmF0aW9u/LmpwZz92ZXI9Ng')
